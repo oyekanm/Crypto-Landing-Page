35d29d39eaa89a94215313ad3405cbf277a28f95
Oyekanmi Boluwatife <enitanboluwatife5@gmail.com>
2022-09-04 17:42:22 +0100

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-import gsap from "gsap";
+// import gsap from "gsap";
+
+const navigation = document.querySelector(".Nav__navigation");
+const nav = document.querySelector(".Nav");
+const cards = document.querySelectorAll(".cards");
+const span = document.querySelectorAll(".span");
+
+navigation.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+  nav.classList.toggle("show");
+});
+
+cards.forEach((card) => {
+  card.addEventListener("mouseover", (e) => {
+    card.classList.add("card__active");
+  });
+});
+cards.forEach((card) => {
+  card.addEventListener("mouseout", (e) => {
+    card.classList.remove("card__active");
+  });
+});
+
+const duration = 5000;
+
+span.forEach((item) => {
+  let startValue = 0;
+  const data = parseInt(item.getAttribute("data-value"));
+  const time = Math.floor(duration / data);
+
+  const interval = setInterval(() => {
+    startValue += 1;
+    item.textContent = startValue;
+    if (startValue === data) {
+      clearInterval(interval);
+    }
+  }, [time]);
+});
